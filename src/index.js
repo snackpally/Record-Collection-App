eb64ec7a250bdeca42424500e66ff7d4ba1ade85
Joel <joelwilsonmt@gmail.com>
2018-10-27 16:56:13 -0600

    Added Notes

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+//create components
+
+
+//search componenet
+
+
+//edit component
+
+
+//Record Component (contains title, artist, genre, album art link, tracklist, release date, label, trackCount)
+
+
+//Record List Component
+
+
+//app component
+
+
+ReactDOM.render(
+  document.getElementById('root');
+);
