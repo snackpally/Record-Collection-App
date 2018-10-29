c181cb642208b4394fed62a4efd4d8f1562d2a2f
Zack Viente <Zack@Zacks-MacBook-Air.local>
2018-10-29 14:19:27 -0600

    This is the correct JS file, once we pull from here I'll delete the development branch and we should be good.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import data from './albums.json';
 //class App extends React.Component {
 //app component
 class App extends React.Component {
-  render() {
-    return <RecordList />;
-  }
+ render() {
+   return <AlbumList />;
+ }
 }
 
 //search componet
@@ -17,77 +17,80 @@ class App extends React.Component {
 
 //edit component
 class Edit extends React.Component{
-  constructor(props){
-    super(props);
-    this.state= {
-      value: 'Edit All',
-    };
-  }
+ constructor(props){
+   super(props);
+   this.state= {
+     value: 'Edit All',
+   };
+ }
 
-    render() {
-      return (
-        <button className="editBtn" onClick={()=> {if (this.state.value === 'Edit All'){
-          this.setState({value: 'Submit'})
-        } else {
-          this.setState({value: 'Edit All'})
-        }
-      }}>
-          {this.state.value}
-        </button>
-      );
-    }
+   render() {
+     return (
+       <button className="editBtn" onClick={()=> {if (this.state.value === 'Edit All'){
+         this.setState({value: 'Submit'})
+       } else {
+         this.setState({value: 'Edit All'})
+       }
+     }}>
+         {this.state.value}
+       </button>
+     );
+   }
 }
 
 //Record List Component
-class RecordList extends React.Component {
+class AlbumList extends React.Component {
 
-  render() {
-    let result = [];
-    for(var i = 0; i < data.length; i++){
-      result.push(<Album data={data[i]} />);
-    }
+ render() {
+   let result = [];
+   for(var i = 0; i < data.length; i++){
+     result.push(<Album data={data[i]} />);
+   }
 
-    return result;
-  }
+   return result;
+ }
 }
 //Record Component (contains title, artist, genre, album art link, tracklist, release date, label, trackCount)
 class Album extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+ // constructor(props) {
+ //   super(props);
+ // }
 
 
-  render() {
+ render() {
 
 
-    var album = this.props.data;
-    var style = {
-      backgroundImage:'url(' + album.album_img_link + ')',
-    };
-    return (
-      <div id="f1_container">
-        <div id="f1_card" className="shadow">
-          <div className="front face" style={style}>
-          </div>
-          <div className="back face center">
-                <ul>
-                  <li><img className="backPic" src={album.album_img_link} alt=""/></li>
-                  <li><h1>{album.title}</h1></li>
-                  <li>{album.artist}</li>
-                  <li>{album.year}</li>
-                  <li>{album.genre}</li>
-                  <li>{album.label}</li>
-                  <li>{album.track_count}</li>
-                  // todo map track list to display key and value
-                  <li>{album.track_list}</li>
-                </ul>
-              <Edit />
-          </div>
-          </div>
-      </div>
-    );
-  }
+   var album = this.props.data;
+   var style = {
+     backgroundImage:'url(' + album.album_img_link + ')',
+   };
+   return (
+     <div id="f1_container">
+       <div id="f1_card" className="shadow">
+         <div className="front face" style={style}>
+         </div>
+         <div className="back face center">
+               <ul>
+                 <li><img className="backPic" src={album.album_img_link} alt=""/></li>
+                 <li><h1>{album.title}</h1></li>
+                 <li>{album.artist}</li>
+                 <li>{album.year}</li>
+                 <li>{album.genre}</li>
+                 <li>{album.label}</li>
+                 <li>{album.track_count}</li>
+                 // todo map track list to display key and value
+                 <li>{album.track_list}</li>
+               </ul>
+             <Edit />
+         </div>
+         </div>
+     </div>
+   );
+ }
 }
+
+
+
 
 
 
@@ -96,6 +99,6 @@ class Album extends React.Component {
 //Dom Render
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
+ <App/>,
+ document.getElementById('root')
 );
