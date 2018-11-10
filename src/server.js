d7eba53c58cfbe6a110c9608417d8bf7a1aa6899
Jeremy Cornell <jeremy.cornell77@gmail.com>
2018-11-10 16:09:35 -0700

    All CRUD complete

--- src/server.js
+++ src/server.js
@@ -34,8 +34,9 @@ app.get('/getAlbumList', function(request, response){
     } else{
       response.send(documents);
     }
-  })
+  });
 });
+
 app.get('/get/:id', function(request, response){
   Album.find({}, function(err, documents){//reach into our database and if error log error else send info
     if(err){
@@ -43,8 +44,9 @@ app.get('/get/:id', function(request, response){
     } else{
       response.send(documents);
     }
-  })
+  });
 });
+
 app.put('/update', function(request, response){
   Album.find({}, function(err, documents){//reach into our database and if error log error else send info
     if(err){
@@ -52,8 +54,9 @@ app.put('/update', function(request, response){
     } else{
       response.send(documents);
     }
-  })
+  });
 });
+
 app.put('/update/:title', function(request, response){
   console.log(request.body);
   Album.findOneAndUpdate({title:request.params.title}, request.body, {new: true}, function(err, documents){//reach into our database and if error log error else send info
@@ -64,18 +67,31 @@ app.put('/update/:title', function(request, response){
      console.log(request.body);
     }
   });
+});
 
-});
-app.delete('/delete/:title', function(request, response){
-  Album.findOneAndRemove({title:request.params.title}, function(err, doc){//reach into our database and if error log error else send info
-    if(err){
-      console.log('I messed up');
-    } else{
-      console.log(doc);
-      console.log('success');
-    }
+  app.delete('/delete/:title', function(request, response){
+    Album.findOneAndRemove({title:request.params.title}, function(err, doc){//reach into our database and if error log error else send info
+      if(err){
+        console.log('I messed up');
+      } else{
+        console.log(doc);
+        console.log('success');
+      }
+    });
   });
+
+app.post('/create', function(request, response){
+  console.log(request.body);
+    var new_album = new Album(request.body);
+    new_album.save(function(err,result) {
+      if(err){
+        console.log(err);
+      } else {
+        response.send(result);
+      }
+    });
 });
+
 
 
 app.listen(3001, function(){
