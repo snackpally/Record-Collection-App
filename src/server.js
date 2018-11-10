2ae71d2a86202cb42f76081bd53e88472c65645e
sharonfuller <sharon.fuller717@gmail.com>
2018-11-10 14:25:01 -0700

    worked on update and delete crud

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); //parses through JSON and body requests
 const cors = require('cors');
 
 let db_url = 'mongodb://127.0.0.1/album_database'; //url to our database in mongoose
@@ -20,13 +20,15 @@ var albumSchema = new mongoose.Schema({
   "track_list": Array
 });
 
-var User = mongoose.model('User', albumSchema, "albums");
+var Album = mongoose.model('album', albumSchema);
 
 const app = express();//Create basic server with express
 app.use(cors());
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 app.get('/getAlbumList', function(request, response){
-  User.find({}, function(err, documents){//reach into our database and if error log error else send info
+  Album.find({}, function(err, documents){//reach into our database and if error log error else send info
     if(err){
       console.log(err);
     } else{
@@ -34,6 +36,48 @@ app.get('/getAlbumList', function(request, response){
     }
   })
 });
+app.get('/get/:id', function(request, response){
+  Album.find({}, function(err, documents){//reach into our database and if error log error else send info
+    if(err){
+      console.log(err);
+    } else{
+      response.send(documents);
+    }
+  })
+});
+app.put('/update', function(request, response){
+  Album.find({}, function(err, documents){//reach into our database and if error log error else send info
+    if(err){
+      console.log(err);
+    } else{
+      response.send(documents);
+    }
+  })
+});
+app.put('/update/:title', function(request, response){
+  console.log(request.body);
+  Album.findOneAndUpdate({title:request.params.title}, request.body, {new: true}, function(err, documents){//reach into our database and if error log error else send info
+    if(err){
+      console.log("I messed up");
+    } else{
+     response.send(request.body);
+     console.log(request.body);
+    }
+  });
+
+});
+app.delete('/delete/:id', function(request, response){
+  Album.findByIdAndRemove({id:request.params.id}, function(err, doc){//reach into our database and if error log error else send info
+    if(err){
+      console.log('I messed up');
+    } else{
+      return response.status(200).send(doc);
+
+      console.log(response);
+    }
+  });
+});
+
 
 app.listen(3001, function(){
   'Server is running on 3001'
